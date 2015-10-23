413d74a52d0c13d7d6a021abcd1161f647943f74
D. Andrei Măceș <dmaces@nd.edu>
2015-10-23 16:14:13 -0400

    ovation: Building AOSP
    Change-Id: I1e4c695818732999ca7e63c432eb8b3b4d709a39

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -27,4 +27,5 @@
 #
 
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_ovation.mk \
     $(LOCAL_DIR)/full_ovation.mk
--- aosp_ovation.mk
+++ aosp_ovation.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2008 The Android Open Source Project
+# Copyright 2013 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,13 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+$(call inherit-product, device/bn/ovation/full_ovation.mk)
+$(call inherit-product-if-exists, device/bn/common/aosp.mk)
 
-# This file is executed by build/envsetup.sh, and can use anything
-# defined in envsetup.sh.
-#
-# In particular, you can add lunch options with the add_lunch_combo
-# function: add_lunch_combo generic-eng
-
-add_lunch_combo full_ovation-eng
-add_lunch_combo cm_ovation-userdebug
-add_lunch_combo aosp_ovation-userdebug
+PRODUCT_NAME := aosp_ovation
+PRODUCT_MODEL := Nook HD+
+PRODUCT_RELEASE_NAME := NookHD+
