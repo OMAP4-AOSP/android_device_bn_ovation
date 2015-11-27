ae2082df64e2b88b1470ce9d345cf343a0866950
D. Andrei Măceș <dmaces@nd.edu>
2015-11-27 16:24:42 -0500

    Revert "Revert "Lower heap size to 402M to please braindead Amazon Kindle app from""
    This reverts commit 4b9fcf0c9e0e175ce8cfd41e8e6545411e07c374.

--- device.mk
+++ device.mk
@@ -49,7 +49,7 @@ ADDITIONAL_BUILD_PROPERTIES += ro.sf.lcd_density=260
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=8m \
     dalvik.vm.heapgrowthlimit=128m \
-    dalvik.vm.heapsize=448m \
+    dalvik.vm.heapsize=402m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=2m \
     dalvik.vm.heapmaxfree=8m
